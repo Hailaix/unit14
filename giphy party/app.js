bd05166d401fde5751c44d7eb38683ab0669daa2
NHower <howernickoli@gmail.com>
2022-07-23 14:26:15 -0400

    returning extracted gif url from api request

--- giphy party/app.js
+++ giphy party/app.js
@@ -1,14 +1,17 @@
 console.log("Let's get this party started!");
+
+
 //http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
+//returns a single url from a random gif object from the search result
 async function giphyrequest(q){
     const res = await axios.get("http://api.giphy.com/v1/gifs/search", { params: {
         q,
-        api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+        api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", //hard coded because I think its the only one we have access to.
     }});
-    console.log(res);
+    const returnurl = res.data.data[Math.floor(Math.random()*res.data.data.length)].url
+    console.log(returnurl);
+    return returnurl;
 }
 
-async function giphytest(){
-    const res = await axios.get("https://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym");
-    console.log(res);
-}
+const imgdiv = document.getElementById('imgdiv');
+const searchform = document.getElementById('searchform');

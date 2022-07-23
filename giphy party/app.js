ed83ebf66dfbfdb3a01b9868b0fee368ed082afa
NHower <howernickoli@gmail.com>
2022-07-23 14:06:53 -0400

    correct axios request format

--- giphy party/app.js
+++ giphy party/app.js
@@ -1,1 +1,14 @@
 console.log("Let's get this party started!");
+//http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
+async function giphyrequest(q){
+    const res = await axios.get("http://api.giphy.com/v1/gifs/search", { params: {
+        q,
+        api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"
+    }});
+    console.log(res);
+}
+
+async function giphytest(){
+    const res = await axios.get("https://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym");
+    console.log(res);
+}

838020efe71625a10b2e9f0cdad1b0ba540f2d1f
NHower <howernickoli@gmail.com>
2022-07-23 14:44:58 -0400

    working? but chrome is blocking img load as CORB

--- giphy party/app.js
+++ giphy party/app.js
@@ -2,7 +2,6 @@ console.log("Let's get this party started!");
 
 
 //http://api.giphy.com/v1/gifs/search?q=hilarious&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym
-//returns a single url from a random gif object from the search result
 async function giphyrequest(q){
     const res = await axios.get("http://api.giphy.com/v1/gifs/search", { params: {
         q,
@@ -15,9 +14,15 @@ async function giphyrequest(q){
 
 const imgdiv = document.getElementById('imgdiv');
 const searchform = document.getElementById('searchform');
-searchform.addEventListener('submit', function(e){
+
+searchform.addEventListener('submit', async function(e){
     e.preventDefault();
-    const imgurl = giphyrequest(e.target['q']);
+    const res = await axios.get("http://api.giphy.com/v1/gifs/search", { params: {
+        q: e.target['q'].value,
+        api_key:"MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym", //hard coded because I think its the only one we have access to.
+    }});
+    console.log(res);
+    const imgurl = res.data.data[Math.floor(Math.random()*res.data.data.length)].url; //random gif url
     const newimg = document.createElement('img');
     newimg.src = imgurl;
     imgdiv.append(newimg);
